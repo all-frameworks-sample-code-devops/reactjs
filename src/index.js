bf41d482fbc4c8881f3d1b368a2aed5ab7febf23
Manda Praneeth Kumar <manda.praneeth72@gmail.com>
2022-07-25 15:28:36 +0530

    react js sample code

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+ function Bunny(){
+   return(
+     <h1>Hello Praneeth</h1>
+   )
+ }
+
+ ReactDom.render(<Bunny/>,document.getElementById('root'))
